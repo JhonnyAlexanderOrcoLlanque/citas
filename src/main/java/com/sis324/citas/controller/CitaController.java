1dca297aeb18d2a156607b5cd86967b2241285b3
JhonnyAlexanderOrcoLlanque <162645556+JhonnyAlexanderOrcoLlanque@users.noreply.github.com>
2024-06-20 06:10:40 -0400

    Update CitaController.java
    hola mundo

--- src/main/java/com/sis324/citas/controller/CitaController.java
+++ src/main/java/com/sis324/citas/controller/CitaController.java
@@ -1,5 +1,5 @@
 package com.sis324.gestionColas.controller;
-
+//hola mundo
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
